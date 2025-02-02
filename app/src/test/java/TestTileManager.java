5a5e20af3181eade862385ad7b4eb0842a8334fa
Tolga Erol <to6644er-s@student.lu.se>
2025-02-02 12:08:26 +0100

    test cases for TileManager

--- app/src/test/java/TestTileManager.java
+++ app/src/test/java/TestTileManager.java
@@ -22,10 +22,27 @@ public class TestTileManager {
     @Test
     public void testgetTileImage(){
         for(int i = 0 ; i < tm.tile.length ; i++){
-            assertTrue(tm.tile[i] != null);
+            assertTrue(tm.tile[i] != null && tm.tile[i] instanceof Tile);
+            
         }
-
+        
     } 
+
+    @Test
+    public void testLoadMap(){
+        tm.loadMap("/maps/map.txt");
+        assertTrue(tm.mapTileNum.length == 50 && tm.mapTileNum[0].length == 50);
+
+        for(int i = 0 ; i < tm.mapTileNum.length ; i++){
+            for(int j = 0 ; j < tm.mapTileNum[0].length ; j++){
+
+                assertTrue(tm.mapTileNum[i][j] >= 0);
+            }
+        }   
+
+    }
+
+
 
 
 }
